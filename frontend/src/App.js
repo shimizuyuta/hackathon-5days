8e0279e3d9bb4c6cc0f2a74a1731ea471ca4c352
yusei0329 <nwys.28@gmail.com>
2022-02-22 12:59:31 +0900

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import react from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Top from "./page/Top";
+import Watch from "./page/Watch";
+import Search from "./page/Search";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Top />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/watch" element={<Watch />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
