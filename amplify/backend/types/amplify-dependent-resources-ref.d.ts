a8e3313c4aefdef0d9b3c58f0dacb262ba174be0
clean_water <shmizuyuta213@icloud.com>
2022-02-23 15:43:53 +0900

    error

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "hackathon2022teambb22f046a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apif0a60b76": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import Top from "./page/Top";
 import Watch from "./components/watch/Watch";
 import "normalize.css";
 
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+
 function App() {
   return (
     <div className="App">
